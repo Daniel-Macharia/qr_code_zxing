952fc594b8ac5f8e32d7a90947e10ae7741f399d
Daniel-Macharia <140121360+Daniel-Macharia@users.noreply.github.com>
2024-05-22 17:10:42 +0300

    QR and Bar code scanning and reading.
    practice project on qr and bar code generation and scanning.

---
+++ QRGenerator/src/qrgenerator/QRGenerator.java
@@ -0,0 +1,30 @@
+
+package qrgenerator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.nio.file.Paths;
+
+public class QRGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String data = "0712696965";
+        String destination = "C:\\Users\\AdminAttachee\\Documents\\NetBeansProjects\\QRGenerator\\src\\qr_code.jpg";
+        
+        try
+        {
+            BitMatrix bm = new MultiFormatWriter().encode( "1", BarcodeFormat.QR_CODE, 600, 600);
+            MatrixToImageWriter.writeToPath( bm, "jpg", Paths.get(destination));
+        }catch( Exception e )
+        {
+            System.out.println("Error: " + e);
+        }
+    }
+    
+}
